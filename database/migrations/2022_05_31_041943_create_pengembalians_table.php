ef8d0a72515a76ea901912ebada6a519886fc539
Syarif15 <dsyarif39@gmail.com>
2022-05-31 13:47:44 +0700

    first commit

---
+++ database/migrations/2022_05_31_041943_create_pengembalians_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengembaliansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengembalians', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('peminjaman_id')->references('id')->on('peminjamen');
+            $table->foreignId('book_id')->references('id')->on('books');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengembalians');
+    }
+}
